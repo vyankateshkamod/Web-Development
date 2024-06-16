449fa52d9e6f20e25858372458bceadab4e8527a
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-16 16:07:35 +0530

    Create script.js

---
+++ 55 Javascript variables/script.js
@@ -0,0 +1,45 @@
+console.log("This is vyankatesh")
+
+var a = 5 ;
+// a = a + 1   Not allowed 
+
+let b = 6 ;
+let c = "RED" ;
+let _a = "Anushka" ;
+
+// var a55 = "sid"      Not allowed 
+
+console.log(a+b+8)
+console.log(typeof a , typeof b , typeofc) ;
+
+{
+    let a = 66 ;
+    console.log(a)
+}
+
+console.log(a)
+
+const a1 = 6 ;
+// a1 = a1 + 1 ;   not allowed because a1 is constant 
+
+let x = "Rahul" ;
+let y = 22 ;
+let z = 3.55 ;
+const p = true ;
+let q = undefined ;
+let r = null ;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let obj = {
+    "name" : "Mahesh" ,
+    "job code" : 4356 ,
+    "is wise" : true
+}
+
+console.log(obj)
+obj.salary = "1 lakh"
+console.log(obj)
+obj.salary = "3 lakh"
+console.log(obj)
