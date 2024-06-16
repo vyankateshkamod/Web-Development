64872ab4856cd2db839c0cdd6bbc835801676e86
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-16 16:07:26 +0530

    Create script.js

---
+++ 54 Intro javascript/script.js
@@ -0,0 +1,24 @@
+alert("Dont try again") 
+
+console.log("code is running")
+console.log("code is also running")
+console.log("code is lookig like a wow")
+
+var a = prompt("Enter Your Number") 
+var isTrue = confirm("Are you sure you want to leave this page")
+
+console.log("Your number is :"+a)
+
+if(isTrue){
+    console.log("Computer is blasting")
+
+}
+else{
+    console.log("computer is not blasting")
+}
+
+// we can style using javascript also 
+
+document.title = "Change title"
+
+document.body.style.backgroundColor = 'red'
