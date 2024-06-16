032213f4fd5d1cb119118f419a6275d6a555ba55
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-16 16:09:25 +0530

    Create script.js

---
+++ 58 functions/script.js
@@ -0,0 +1,8 @@
+function fun_name(name) {
+    console.log("hey " + name + " you are nice")
+    console.log("hey " + name + " you are good")
+    console.log("hey " + name + " your good looking")
+    console.log("hey " + name + " whats going onn !")
+}
+
+fun_name("Vyankatesh")
