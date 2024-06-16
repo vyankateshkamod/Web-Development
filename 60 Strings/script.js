04b0f8e67177e65cf3ae866103ce08eccda4fef4
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-16 16:09:31 +0530

    Create script.js

---
+++ 60 Strings/script.js
@@ -0,0 +1,26 @@
+console.log("This is string section")
+let a = "Vyankatesh"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+
+console.log(a.length)
+
+let real_name = "Piyush"
+let friend = "Mohit"
+console.log("his name is " + real_name + " and his friend name is " + friend)
+console.log(`his name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Soham"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+
+console.log(b.replace("So","77"))
+console.log(b.concat(a))
+console.log(b.concat(a,"Rahul","Vidya"))
+
+console.log(b)
