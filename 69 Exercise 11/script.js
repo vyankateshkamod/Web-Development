b10932ee6d9d0b05cba3e1cbf28a563e1f2e3a00
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-20 12:51:09 +0530

    Create script.js

---
+++ 69 Exercise 11/script.js
@@ -0,0 +1,18 @@
+let a = 4
+
+function factorial(number){
+    let arr = Array.from(Array(number+1).keys())
+    let c = arr.slice(1,).reduce((a, b)=> a*b )
+    return c
+ }
+ console.log(factorial(a))
+
+//normal method 
+ function fac(number){
+    let fac =1 
+    for (let index = 1; index <= number; index++) {
+        fac = fac*index;
+    }
+    return fac 
+ }
+ console.log(fac(a))
