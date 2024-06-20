b73ab3d92bbe28d0499d9874fae80165eb79d2d9
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-20 12:50:59 +0530

    Update script.js

--- 67 DOM-children,parents,siblings/script.js
+++ 67 DOM-children,parents,siblings/script.js
@@ -1,1 +1,7 @@
 console.log("Hello World")
+
+console.log(document.body.firstElementChild )
+console.log(document.body.firstElementChild.childNodes )
+console.log(document.body.firstElementChild.children)
+
+console.log(document.body.children[1].rows)
