a81876844d3e46b4f05bde7918ef589a9e69c235
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-20 12:51:27 +0530

    Create script.js

---
+++ 74 Event,Event Bubbling,setinterval,settimeout/script.js
@@ -0,0 +1,14 @@
+let button = document.getElementById("btn")
+//List of all mouse events 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+
+// buttton.addEventListener("dblclick",()=>{
+//     document.querySelector(".box").innerHTML="<b>Yeyy you were clicked</b> Enjoy your click!"
+// })
+
+button.addEventListener("contextmenu",()=>{
+    alert("Dont hack us by right click please ")
+})
+button.addEventListener("keydown",(e)=>{
+    console.log(e,e.key,e.keycode)
+})
