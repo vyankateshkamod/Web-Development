057f29e1e146e5e410533c3e40d4494e53366013
vyankateshkamod <vyankateshkamod@gmail.com>
2024-06-22 23:25:53 +0530

    Create script.js

---
+++ 80 OOP/script.js
@@ -0,0 +1,50 @@
+// let obj={
+//     a:1,
+//     b:"Hello"
+// }
+
+// console.log(obj)
+
+// let Animal = {
+//     eats : true 
+// }
+
+// let rabbit = {
+//     jumps : true 
+// }
+
+// rabbit.__proto__ = Animal   // set reabbit.[[prototype]] = Animal
+
+
+class Animal{
+    constructor(name){
+        this.name = name 
+        console.log("Object is created ...")
+    }
+
+    eats(){
+        console.log("Kha raha hoo ")
+    }
+
+    jumps(){
+        console.log("Kood raha hoo")
+    }
+}
+
+class Lion extends Animal {
+    constructor(name){
+        super(name)
+        console.log("Object is creted and he is lion ")
+    }
+
+    eats(){
+        super.eats()
+        console.log("Kha raha hoo roar")
+    }
+}
+
+let a = new Animal("Peackok")
+console.log(a)
+
+let l = new Lion("Shera")
+console.log(l)
